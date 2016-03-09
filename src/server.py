dc1a47f5b9fbf1dc6640de416e3bd2fe546cfd58
Will Weatherford <weatherford.william@gmail.com>
2016-03-09 10:58:49 -0800

    added comments for to-dos

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Server module to communicate with client module."""
+"""Server module to to take HTTP requests."""
 import sys
 import socket
 
@@ -8,14 +8,32 @@ BUFFER_LENGTH = 8
 ADDRINFO = ('127.0.0.1', 5000)
 
 
+# TODO: update response assemble and tests for more sophisticated
+#   testing of each component. Assert for required parts while disrgarding
+#   optional components (but making sure they are in the right place).
+#   split on \r\n\r\n assert there are 2 pieces
+#   for first line of response, use maxsplit arg to split
+#   e.g. "HTTP/1.1 500 Internal Server Error".split(' ', maxsplit=2)
+#   >>> ["HTTP/1.1", "500", "Internal Server Error"]
+
+
 def server():
-    """Start a server using a socket at ('127.0.0.1', 5000)."""
+    """Start a server using a socket at server.ADDRINFO."""
+    # TODO: explicitly state keyword args initializing socket.socket()
     serv_sock = socket.socket(proto=socket.IPPROTO_TCP)
+    # TODO:
+    # use socket.setsockopt to allow to re-use server right away
+    #   instead of waiting to use same IP/port again
     serv_sock.bind(ADDRINFO)
     try:
         while True:
+            # TODO: move this outside while loop
+            # This just sets the max number of connections this server
+            #   can talk to simultaneously
             serv_sock.listen(1)
             conn, addr = serv_sock.accept()
+
+            # TODO: Build a list instead and then use the b''.join() method
             req = b''
             while True:
                 part = conn.recv(BUFFER_LENGTH)
@@ -28,6 +46,10 @@ def server():
             conn.sendall(response_ok())
             conn.close()
     except KeyboardInterrupt:
+        # TODO: find a way to see if conn is an existing name
+        #   if so, close.
+        # try conn.close()/except name error
+        # move server shutdown to finally statement - always shutdown
         print('\nShutting down the server...\n')
         serv_sock.close()
         sys.exit()
